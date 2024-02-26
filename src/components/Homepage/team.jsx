155b30f0c6e291cd6316fa19f2b9e40a44e0469f
Anurag <anuragsah401@gmail.com>
2024-02-26 18:27:28 +0545

    working on team section

--- src/components/Homepage/team.jsx
+++ src/components/Homepage/team.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { FaAngleRight } from "react-icons/fa6";
+import { FiPhone } from "react-icons/fi";
+import { LuMail } from "react-icons/lu";
+
 import Container from "components/UI/Container";
 import SectionTitle from "components/UI/SectionTitle";
 import Image from "next/image";
@@ -37,7 +41,10 @@ const Team = () => {
         <div className="flex gap-10 justify-center items-center flex-wrap">
           {teamDetails.map((team, i) => {
             return (
-              <div key={i}>
+              <div
+                key={i}
+                className="flex flex-col items-center border border-[#EBECEC] group"
+              >
                 <Image
                   src={team.pic}
                   alt={team.name}
@@ -45,11 +52,28 @@ const Team = () => {
                   height={300}
                   className="object-cover"
                 />
-                <div className="text-center mt-[20px] mb-[30px]">
-                  <h3 className="font-semibold text-[1.375em] mb-1">
+                <div className="text-center pt-[20px] animate group-hover:text-white group-hover:bg-[#333] w-[100%] h-[100%]">
+                  <h3 className="font-semibold text-[1.375em] mb-1 ">
                     {team.name}
                   </h3>
-                  <span className="text-primary">{team.post}</span>
+                  <div className="mb-3 text-primary group-hover:text-white animate ">
+                    {team.post}
+                  </div>
+
+                  <div className="flex justify-center gap-2 items-center mb-1">
+                    <FiPhone size={"1.3em"} />
+                    {team.phoneNo}
+                  </div>
+
+                  <div className="mb-5 flex justify-center gap-2 items-center">
+                    <LuMail />
+                    {team.email}
+                  </div>
+
+                  <button className="rounded-sm -mb-5 w-[50%] mx-auto animate py-3 bg-primary text-white hover:bg-secondary flex justify-center items-center gap-1">
+                    <span>View Profile</span>
+                    <FaAngleRight />
+                  </button>
                 </div>
               </div>
             );
